75c8d47e3926a5cd9f4598b5a45aac6071da80ed
Deep-Patel2002 <131765055+Deep-Patel2002@users.noreply.github.com>
2023-06-02 16:56:55 +0530

    Add files via upload

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Policy = () => {
+  return (
+    <Layout title={'Privacy Policy'}>
+    <h1>Page not found</h1>
+  </Layout>
+  )
+}
+
+export default Policy
